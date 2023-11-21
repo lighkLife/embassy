157cdc615d042359a322ea22cb0bf545001005d9
lighkLife <lighkLife@users.noreply.github.com>
2023-11-21 10:07:04 +0000

    deploy: 2371bf739ad94d841f491675c7881289aeba685e

--- embassy_sync/sidebar-items.js
+++ embassy_sync/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["blocking_mutex","channel","mutex","pipe","pubsub","signal","waitqueue","zerocopy_channel"]};
+window.SIDEBAR_ITEMS = {"mod":["blocking_mutex","channel","mutex","pipe","priority_channel","pubsub","signal","waitqueue","zerocopy_channel"]};
--- src-files.js
+++ src-files.js
@@ -8,7 +8,7 @@ var srcIndex = JSON.parse('{\
 "embassy_executor":["",[["arch",[],["std.rs"]],["raw",[],["mod.rs","run_queue_atomics.rs","state_atomics.rs","util.rs","waker_turbo.rs"]]],["fmt.rs","lib.rs","spawner.rs"]],\
 "embassy_futures":["",[],["block_on.rs","fmt.rs","join.rs","lib.rs","select.rs","yield_now.rs"]],\
 "embassy_macros":["",[["macros",[],["main.rs","mod.rs","task.rs"]],["util",[],["ctxt.rs","mod.rs"]]],["lib.rs"]],\
-"embassy_sync":["",[["blocking_mutex",[],["mod.rs","raw.rs"]],["pubsub",[],["mod.rs","publisher.rs","subscriber.rs"]],["waitqueue",[],["atomic_waker_turbo.rs","mod.rs","multi_waker.rs","waker_registration.rs"]]],["channel.rs","fmt.rs","lib.rs","mutex.rs","pipe.rs","ring_buffer.rs","signal.rs","zerocopy_channel.rs"]],\
+"embassy_sync":["",[["blocking_mutex",[],["mod.rs","raw.rs"]],["pubsub",[],["mod.rs","publisher.rs","subscriber.rs"]],["waitqueue",[],["atomic_waker_turbo.rs","mod.rs","multi_waker.rs","waker_registration.rs"]]],["channel.rs","fmt.rs","lib.rs","mutex.rs","pipe.rs","priority_channel.rs","ring_buffer.rs","signal.rs","zerocopy_channel.rs"]],\
 "embassy_time":["",[],["delay.rs","driver.rs","driver_std.rs","duration.rs","fmt.rs","instant.rs","lib.rs","queue.rs","tick.rs","timer.rs"]],\
 "embedded_hal":["",[],["delay.rs","digital.rs","i2c.rs","lib.rs","pwm.rs","spi.rs"]],\
 "embedded_hal_async":["",[],["delay.rs","digital.rs","i2c.rs","lib.rs","spi.rs"]],\
